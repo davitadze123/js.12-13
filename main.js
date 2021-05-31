0ebbf954ebb2a4463597d53f287b450312a25297
marika davitadze <83085324+davitadze123@users.noreply.github.com>
2021-05-31 02:15:26 -0800

    index.html

---
+++ main.js
@@ -0,0 +1,12 @@
+//decimalToBinary //
+
+function num(n){
+    return Number(n.toString(2));
+  }
+  console.log(num(500));
+
+
+//binaryToDecimal //
+var binary='11111111';
+var digit=parseInt(binary,2)
+console.log(digit);
